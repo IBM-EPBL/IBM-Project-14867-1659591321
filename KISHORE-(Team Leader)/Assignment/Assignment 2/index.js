ad5ccd449ba4e08b49b4dc153f0b8a3de2a8df04
KishoreS112 <104120900+KishoreS112@users.noreply.github.com>
2022-11-10 19:22:01 +0530

    Add files via upload

---
+++ KISHORE-(Team Leader)/Assignment/Assignment 2/index.js
@@ -0,0 +1,27 @@
+function Validate () {
+    let table=document.getElementById('table');
+    var row = table.insertRow(0);
+
+    var name = row.insertCell(0);
+    var email = row.insertCell(1);
+    var pass = row.insertCell(2);
+    var radio = row.insertCell(3);
+    var drop = row.insertCell(4);
+    var textarea = row.insertCell(5);
+
+    name.innerHTML = document.getElementById("fname").value;
+    email.innerHTML = document.getElementById("email").value;
+    pass.innerHTML =document.getElementById("pass").value;
+    radio.innerHTML = document.getElementById("fav_language").value;
+    drop.innerHTML = document.getElementById("cars").value;
+    textarea.innerHTML = document.getElementById("tarea").value;
+
+
+    alert("success");
+
+    
+    
+    return false;
+}
+
+
